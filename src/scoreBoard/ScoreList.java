4e9b77e9da16d413cad79801c0a5982c991333c3
turn <kksy2423@naver.com>
2022-03-28 00:06:54 +0900

    feat: define scorelist

---
+++ src/scoreBoard/ScoreList.java
@@ -0,0 +1,40 @@
+package scoreBoard;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class ScoreList{
+    private List<User> list = new ArrayList<>();
+
+    private static ScoreList scoreList = new ScoreList();
+
+    //싱글톤
+    public static ScoreList getInstance() {
+        return scoreList;
+    }
+
+    public List<User> getList() {
+        return list;
+    }
+
+    public List<User> push(User user){
+        this.list.add(user);
+        return this.list;
+    }
+
+    public void deleteAll(){
+        this.list.clear();
+    }
+
+    public List<User> sortDescByScore(){
+        Collections.sort(list);
+        return this.list;
+    }
+
+
+
+}

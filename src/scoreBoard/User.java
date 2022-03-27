1dadfa8cacdd58f083b0a95a033939d44c261bd1
turn <kksy2423@naver.com>
2022-03-27 23:35:34 +0900

    feat: define user

---
+++ src/scoreBoard/User.java
@@ -0,0 +1,24 @@
+package scoreBoard;
+
+public class User {
+
+    private String name;
+
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}

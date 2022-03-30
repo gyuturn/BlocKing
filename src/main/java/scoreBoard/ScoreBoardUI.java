b3006d425429d12b73bc789881a8501aa1d519fb
gyuturn <rbtjrrbals@gmail.com>
2022-03-30 14:39:12 +0900

    feat: ScoreBoardUI (사용자 점수 입력 전)

--- src/main/java/scoreBoard/ScoreBoardUI.java
+++ src/main/java/scoreBoard/ScoreBoardUI.java
@@ -1,19 +1,32 @@
 package scoreBoard;
 
+import setting.FixSizeUI;
 import setting.ScreenSize;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ScoreBoardUI extends JFrame{
 
     private JPanel mainPanel;
     private ScreenSize screenSize = ScreenSize.getInstance();
+    private ScoreList scoreList = ScoreList.getInstance();
+    //각 label 및 텍스트 위치 설정 (배경 크기 기준)
+    private int height = screenSize.getHeight();
+    private int width = screenSize.getWidth();
+
+    private JPanel scorePanel;
+    private JButton inputBtn;
+    private JButton exitBtn;
 
 
 
-    public ScoreBoardUI(){
+    //게임이 끝난 후 사용자에게 점수입력을 받기위해 생성자 parameter로 설정
+    public ScoreBoardUI(int score){
         //JFrame setting
         super("software-tetris");//제목
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 메모리까지 종료
@@ -29,10 +42,84 @@ public class ScoreBoardUI extends JFrame{
                 BorderFactory.createLineBorder(Color.DARK_GRAY, 5)
         );
         mainPanel.setBorder(border);
-
         this.getContentPane().add(mainPanel,BorderLayout.CENTER);
 
+        showScoreList();
+        showInputBtn(score);
 
+
+
+
+
+
+    }
+
+
+
+    private void showInputBtn(int score) {
+         inputBtn = new JButton("점수 입력");
+         exitBtn = new JButton("시작 화면");
+
+        scorePanel.add(inputBtn);
+        scorePanel.add(new JLabel(" "));
+        scorePanel.add(exitBtn);
+        scorePanel.add(new JLabel(" "));
+
+        //사용자의 이름 및 점수 입력 받음
+        inputBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ScoreInputUI(score);
+                setVisible(false);
+            }
+        });
+
+    }
+
+    private void showScoreList() {
+        //스코어 리스트 최대 10개까지 설정
+        int listToIndex;
+        if (scoreList.getList().size() >= 10) {
+            listToIndex=10;
+        }
+        else{
+            listToIndex = scoreList.getList().size();
+        }
+
+
+        scorePanel = new JPanel();
+        scorePanel.setBackground(Color.BLACK);
+        GridLayout gridLayout = new GridLayout(listToIndex+4, 2,1,height/30);
+        scorePanel.setLayout(gridLayout);
+        scorePanel.setBorder(BorderFactory.createEmptyBorder(10, 70, 10, 0));
+
+
+        JLabel titleLabel = new JLabel("   스코어 리스트");
+        titleLabel.setForeground(Color.WHITE);
+        scorePanel.add(titleLabel);
+        scorePanel.add(new JLabel(" "));
+
+        JLabel nameLabel = new JLabel("이름");
+        nameLabel.setForeground(Color.WHITE);
+        scorePanel.add(nameLabel);
+        JLabel scoreLabel = new JLabel("점수");
+        scoreLabel.setForeground(Color.WHITE);
+        scorePanel.add(scoreLabel);
+
+
+
+        for (int i = 0; i < listToIndex; i++) {
+
+            JLabel userNameLabel = new JLabel(scoreList.getList().get(i).getName());
+            userNameLabel.setForeground(Color.WHITE);
+            scorePanel.add(userNameLabel);
+
+            JLabel userScoreLabel = new JLabel(Integer.toString(scoreList.getList().get(i).getScore()));
+            userScoreLabel.setForeground(Color.WHITE);
+            scorePanel.add(userScoreLabel);
+        }
+
+        mainPanel.add(scorePanel,CENTER_ALIGNMENT);
     }
 
 }

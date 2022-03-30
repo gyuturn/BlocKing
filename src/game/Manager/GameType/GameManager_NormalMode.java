4fa2785a8fd2921e43862a6d8f4aa422ed41b878
Alex <77562357+oeccsy@users.noreply.github.com>
2022-03-30 23:11:36 +0900

    feat : GameManager_NormalMode 싱글톤
    #region Singleton 참고

--- src/game/Manager/GameType/GameManager_NormalMode.java
+++ src/game/Manager/GameType/GameManager_NormalMode.java
@@ -1,5 +1,38 @@
 package game.Manager.GameType;
 
-public class GameManager_NormalMode {
+import game.Manager.GameManager;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import game.Model.BlockController;
+
+public class GameManager_NormalMode extends GameManager {
+
+    private boolean isGameOver = false;
+    private boolean isBlockStop = false;
+    private static int timeScale = 1000;
+    private int blockCount = 0;
+    private int lineCount = 0;
+    private int score = 0;
+
+    private BlockController curBlock;
+
+    Timer timer;
+    private KeyListener interaction;
+
+    //#region Singleton
+
+    private static GameManager_NormalMode instance = new GameManager_NormalMode();
     
+    public GameManager_NormalMode getInstance() {
+        return instance;
+    }
+
+    //#endregion
+
+    
+
 }

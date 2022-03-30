8c44f6ac745238f75b87eb6486f90490353089ea
Alex <77562357+oeccsy@users.noreply.github.com>
2022-03-30 23:16:22 +0900

    feat : 게임 동작 흐름 설계
    #region GameFramework

--- src/game/Manager/GameType/GameManager_NormalMode.java
+++ src/game/Manager/GameType/GameManager_NormalMode.java
@@ -33,6 +33,84 @@ public class GameManager_NormalMode extends GameManager {
 
     //#endregion
 
+    //#region GameFramework
+
     
+    @Override
+    protected void GameFramework() { //전체적인 게임의 동작 흐름
+        GameReady();
+        StartTimer();
+
+        while(!isGameOver)
+        {
+            CreateNewBlock();   //블록 생성
+            CheckBlockConfirm(); //블록이 바닥에 닿으면 return 합니다.
+
+            SetGameBalance(); //게임 진행도에 따라 블럭 속도 조절
+        }
+
+        GameOver();
+    }
+
+
+    private void GameReady() {
+        //게임을 준비합니다.
+    }
+
+    private void StartTimer() {
+
+        timer = new Timer(timeScale, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				OneFrame();
+			}
+		});
+
+        timer.start();
+    }
+
+    private void CreateNewBlock() {
+        //curBlock = BlockGenerator.getInstance().CreateBlock();
+        blockCount++;
+    }
+
+    private void CheckBlockConfirm() {
+        while(!isGameOver)
+        {
+            if(isBlockStop) return;
+        }
+    }
+
+    private void SetGameBalance() {
+        //일정 수 이상 블록이 삭제되면 떨어지는 속도가 증가합니다.
+        if(timeScale < blockCount / 10)
+        {
+            timeScale--;
+        }
+        //일정 수 이상 줄이 삭제되면 떨어지는 속도가 증가합니다.
+        if(timeScale < lineCount / 10)
+        {
+            timeScale--;
+        }
+    }
+
+    
+    @Override
+    protected void GameOver() {
+        //게임이 종료되면 호출됩니다.
+    }
+
+    @Override
+    public void StartGameFramework() {
+        //GameFramework를 시작
+    }
+
+    @Override
+    public void StopGameFramework() {
+        //GameFramework를 멈추고 싶은 경우
+        //일시정지 등등
+    }
+    
+    //#endregion
 
 }

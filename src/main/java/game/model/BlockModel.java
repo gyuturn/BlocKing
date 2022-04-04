d274600c7074a4031971b1e84950350e8eb10d74
YunSeong <77562357+oeccsy@users.noreply.github.com>
2022-04-04 21:28:14 +0900

    feat : block에 pos data 추가

--- src/main/java/game/model/BlockModel.java
+++ src/main/java/game/model/BlockModel.java
@@ -4,8 +4,11 @@ import java.awt.Color;
 
 public abstract class BlockModel {
 
-    protected int[][] shape;
+    public int[][] shape;
     protected Color color;
+
+    public int posRow = 0;
+    public int posCol = 5;
     
     /* 필요한가? 확인할것
     public BlockModel() {

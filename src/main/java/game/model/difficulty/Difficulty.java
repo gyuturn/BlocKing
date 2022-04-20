dc93a0ee0cbeeced6a388ce2b6455d7cff04b0f5
YunSeong <77562357+oeccsy@users.noreply.github.com>
2022-04-21 00:52:00 +0900

    feat : 난이도 / 모드 종류

---
+++ src/main/java/game/model/difficulty/Difficulty.java
@@ -0,0 +1,36 @@
+package game.model.difficulty;
+
+public class Difficulty {
+
+    private int addSpeed;
+
+    private double perIBlock;
+    private double perJBlock;
+    private double perLBlock;
+    private double perOBlock;
+    private double perSBlock;
+    private double perTBlock;
+    private double perZBlock;
+
+    public Difficulty(
+        int addSpeed,
+
+        double perIBlock,
+        double perJBlock,
+        double perLBlock,
+        double perOBlock,
+        double perSBlock,
+        double perTBlock,
+        double perZBlock) {
+
+            this.addSpeed = addSpeed;
+
+            this.perIBlock = perIBlock;
+            this.perJBlock = perJBlock;
+            this.perLBlock = perLBlock;
+            this.perOBlock = perOBlock;
+            this.perSBlock = perSBlock;
+            this.perTBlock = perTBlock;
+            this.perZBlock = perZBlock;
+    }
+}
